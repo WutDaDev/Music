157d5ee2a4f8c0d77635f798098f4d9047a24930
WutDaDev <brinh250@gmail.com>
2026-06-30 17:50:09 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "discord-selfbot-manager",
+  "version": "1.0.0",
+  "description": "Selfbot to manage music and track user uptime",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "keywords": [
+    "discord",
+    "selfbot",
+    "music"
+  ],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "discord.js-selfbot-v13": "^3.0.0",
+    "dotenv": "^16.4.5"
+  }
+}
